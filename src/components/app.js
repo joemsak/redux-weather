4e144f98cca54af08f92419c72f85fb7d981db42
Joe Sak <joe@joesak.com>
2016-02-21 16:30:49 -0500

    Add rudimentary controlled input search bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Component } from 'react';
 
+import SearchBar from '../containers/search-bar';
+
 export default class App extends Component {
   render() {
-    return (
-      <div>React simple starter</div>
-    );
+    return <div>
+      <SearchBar />
+    </div>
   }
 }
